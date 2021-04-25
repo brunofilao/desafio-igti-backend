b5a09f4589a3f3857c4f4692424a644ac8d672dc
Bruno Carriel <bruno_filao_@hotmail.com>
2021-04-25 18:03:51 -0300

    Adicionando as Rotas2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import gradeRouter from './routes/gradeRouter.js';
+import { gradeRouter } from './routes/gradeRouter.js';
 import cors from 'cors';
 
 import { db } from './models/index.js';
