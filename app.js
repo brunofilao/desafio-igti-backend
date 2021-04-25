90fb2c77a26d2b5cf28cc258bf6e5567f563df80
Bruno Carriel <bruno_filao_@hotmail.com>
2021-04-25 19:18:41 -0300

    Cors

--- app.js
+++ app.js
@@ -24,11 +24,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', gradeRouter);
-app.use(
-  cors({
-    origin: 'http://localhost:8080',
-  })
-);
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*');
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('API em execucao');
